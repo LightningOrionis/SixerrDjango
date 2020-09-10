5be5bd59f41a62447051921b628e568633a1a8e2
LightningOrionis <krasovsky17igor@gmail.com>
2020-09-11 01:49:25 +0300

    update gig_detail

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -16,5 +16,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 application = get_wsgi_application()
 
 
-#from django.contrib.staticfiles.handlers import StaticFilesHandler
-#application = StaticFilesHandler(get_wsgi_application())
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
